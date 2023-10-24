e9c396c386043eb3241c2fe00042326e9063e9bd
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2023-10-24 10:40:12 -0400

    changes

--- main.tf
+++ main.tf
@@ -92,6 +92,13 @@ resource "github_repository" "sonsofphp" {
   has_issues             = true
   has_projects           = false
   has_wiki               = false
+  allow_merge_commit     = false
+  allow_rebase_merge     = false
+
+  allow_squash_merge          = true
+  squash_merge_commit_title   = "PR_TITLE"
+  squash_merge_commit_message = "PR_BODY"
+
   pages {
     cname = "docs.sonsofphp.com"
     source {
