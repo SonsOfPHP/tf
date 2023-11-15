93185a2f8a478fdb79c5f4fddae1be5595f88694
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2023-11-14 20:42:37 -0500

    pager contract repo (#11)

--- main.tf
+++ main.tf
@@ -60,6 +60,12 @@ variable "github_readonly_repos" {
       homepage_url = "https://docs.sonsofphp.com/contracts/money"
       topics       = ["sonsofphp", "sons-of-php", "php", "money"]
     },
+    pager-contract = {
+      name         = "pager-contract"
+      description  = "[read-only] Pager Contacts"
+      homepage_url = "https://docs.sonsofphp.com/contracts/pager"
+      topics       = ["sonsofphp", "sons-of-php", "php", "pager", "pagination"]
+    },
     version_contract = {
       name         = "version-contract"
       description  = "[read-only] Version Contacts"
@@ -194,14 +200,13 @@ variable "github_sonsofphp_labels" {
   type = map(object({
     name        = string
     color       = string
-    description = string
+    #description = string
   }))
 
   default = {
-    Filesystem = {
-      name        = "Filesystem"
-      color       = "5319e7"
-      description = "Filesystem Component"
+    Pager = {
+      name  = "Pager"
+      color = "5319e7"
     }
   }
 }
@@ -326,13 +331,13 @@ resource "github_repository" "sonsofphp" {
 ###
 # Labels for the Mother Repo
 # @see https://registry.terraform.io/providers/integrations/github/latest/docs/resources/issue_label
-#resource "github_issue_label" "sonsofphp" {
-#    for_each    = var.github_sonsofphp_labels
-#    repository  = "sonsofphp"
-#    name        = each.key
-#    color       = each.value.color
-#    description = each.value.description
-#}
+resource "github_issue_label" "sonsofphp" {
+    for_each    = var.github_sonsofphp_labels
+    repository  = "sonsofphp"
+    name        = each.key
+    color       = each.value.color
+    #description = each.value.description
+}
 
 # Read Only Repositories for projects
 resource "github_repository" "readonly" {
